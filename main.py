847e04561d255b11f464be90b93b9b920c7267c2
TonightIwanttosleep <93720107+Napattnpp@users.noreply.github.com>
2025-03-12 21:37:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from Module.bb64u8 import BB64U8
 bb64u8 = BB64U8()
 
 bb64u8.encode('path_of_imgage_to_encode.jpg')
-bb64u8.saveTextImg('...2B64U8/Output/encode.txt', bb64u8.base64_img)
+bb64u8.saveTextImg('path_to_save_encode.txt', bb64u8.base64_img)
 
 print("UTF-8")
 print(bb64u8.utf8_img)
 
-bb64u8.decode('...2B64U8/Output/decode_img.png', bb64u8.base64_img)
+bb64u8.decode('path_to_save_decode_img.png', bb64u8.base64_img)
