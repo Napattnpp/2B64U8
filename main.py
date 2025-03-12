acc146cc20a1e0cb378792f8a32320e520c4a91f
TonightIwanttosleep <93720107+Napattnpp@users.noreply.github.com>
2025-03-12 21:31:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ bb64u8.saveTextImg('...2B64U8/Output/encode.txt', bb64u8.base64_img)
 print("UTF-8")
 print(bb64u8.utf8_img)
 
-bb64u8.decode("...2B64U8/Output/decode_img.png", bb64u8.base64_img)
+bb64u8.decode('...2B64U8/Output/decode_img.png', bb64u8.base64_img)
